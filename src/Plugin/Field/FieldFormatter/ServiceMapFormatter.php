ab0b2a377c486199bafee956008cded606a29376
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-06-14 17:10:57 +0300

    UHF-1083: How about converting the setting to a variable...

--- src/Plugin/Field/FieldFormatter/ServiceMapFormatter.php
+++ src/Plugin/Field/FieldFormatter/ServiceMapFormatter.php
@@ -94,6 +94,9 @@ final class ServiceMapFormatter extends FormatterBase {
     if (!$entity instanceof Unit) {
       throw new \InvalidArgumentException('The "service_map_embed" field can only be used with tpr_unit entities.');
     }
+
+    $link_title = $this->getSetting('link_title');
+
     foreach ($items as $delta => $item) {
       $element[$delta] = [
         'iframe' => [
@@ -109,7 +112,7 @@ final class ServiceMapFormatter extends FormatterBase {
         'link' => [
           '#type' => 'html_tag',
           '#tag' => 'a',
-          '#value' => t((string) $this->getSetting('link_title')),
+          '#value' => t((string) $link_title),
           '#attributes' => [
             'href' => $this->generateUrl($entity),
             'target' => $this->getSetting('target') ? '_blank' : '',

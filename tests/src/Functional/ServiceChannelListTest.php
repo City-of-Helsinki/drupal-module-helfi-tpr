138d006d2f5522d5db49f0b485b77beb870245d0
Tapio Pietinsalo <tapio.pietinsalo@siili.com>
2021-08-24 15:58:07 +0300

    UHF-1772: Add views for TPR Errand services and TPR Service channels
    Adds the missing content listing views for `TPR Errand services` and
    `TPR Service channels`. Users with the `Access remote entities overview
    page` permission are now able to view the listings.
    
    Also adds publish and unpublish actions for `Errand service` and
    `Service channel` entities.

---
+++ tests/src/Functional/ServiceChannelListTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\helfi_tpr\Functional;
+
+use Drupal\Tests\helfi_api_base\Functional\MigrationTestBase;
+use Drupal\Tests\helfi_api_base\Traits\ApiTestTrait;
+
+/**
+ * Tests entity list functionality.
+ *
+ * @group helfi_tpr
+ */
+class ServiceChannelListTest extends MigrationTestBase {
+
+  use ApiTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'views',
+    'helfi_tpr',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests collection route (views).
+   */
+  public function testList() : void {
+    // Make sure anonymous user can't see the entity list.
+    $this->drupalGet('/admin/content/integrations/tpr-service-channel');
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Make sure logged in user without permissions can't see the entity list.
+    $account = $this->createUser();
+    $this->drupalLogin($account);
+    $this->drupalGet('/admin/content/integrations/tpr-errand-service');
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Make sure logged in user with access remote entities overview permission
+    // can see the entity list.
+    $account = $this->createUser([
+      'access remote entities overview'
+    ]);
+    $this->drupalLogin($account);
+    $this->drupalGet('/admin/content/integrations/tpr-service-channel');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('No results found.');
+  }
+
+}

f6e01154693befbfbcdb4ae30243656675e3b629
Juho Lehmonen <juho.lehmonen@siili.com>
2022-03-14 10:51:53 +0200

    UHF-4643: Add custom status or user permission filter for TPR views.

---
+++ src/Plugin/views/filter/Status.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\helfi_tpr\Plugin\views\filter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * Filter by published status.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("tpr_status")
+ */
+class Status extends FilterPluginBase {
+
+  public function adminSummary() {}
+
+  protected function operatorForm(&$form, FormStateInterface $form_state) {}
+
+  public function canExpose() {
+    return FALSE;
+  }
+
+  public function query() {
+    $table = $this->ensureMyTable();
+    $snippet = "$table.content_translation_status = 1 OR ***VIEW_UNPUBLISHED_TPR_ENTITIES*** = 1";
+    $this->query->addWhereExpression($this->options['group'], $snippet);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    $contexts = parent::getCacheContexts();
+
+    $contexts[] = 'user';
+
+    return $contexts;
+  }
+
+}
